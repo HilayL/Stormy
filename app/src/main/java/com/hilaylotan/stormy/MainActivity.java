5664fa4d66c9cfe4a8429dd364dac1fb1242d043
HilayL <hilay.lotan@gmail.com>
2019-11-05 02:31:26 +0200

    Location almos finished, problem with getting latitude and longtitude when starting the app

--- app/src/main/java/com/hilaylotan/stormy/MainActivity.java
+++ app/src/main/java/com/hilaylotan/stormy/MainActivity.java
@@ -13,10 +13,14 @@ import okhttp3.Response;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
@@ -28,6 +32,8 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -42,31 +48,63 @@ import org.json.JSONObject;
 import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG=MainActivity.class.getSimpleName();
+    public String city;
 
     private CurrentWeather currentWeather;
 
     private ImageView iconImageView;
 
-    String latitude;
-    String longitude;
+    public static final int requestPermissionCode=1;
+
+    private GoogleApiClient client;
+    public FusedLocationProviderClient fusedLocationProviderClient;
+
+    private double[] arry=new double[2];
+
+    double latitude;
+    double longitude;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        client = new GoogleApiClient.Builder(this
+        ).addApi(LocationServices.API).build();
+        client.connect();
 
-        latitude = "37.8267";
-        longitude = "-122.4233";
+        if (ActivityCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermission();
+        }
+
+        latitude=getLatitude();
+        longitude =getLongitude();
 
         getForcast(latitude,longitude);
         Log.d(TAG,"working");
     }
 
-    private void getForcast(String latitude, String longitude) {
+    private void requestPermission()
+    {
+        ActivityCompat.requestPermissions
+                (MainActivity.this,new String[]{ACCESS_FINE_LOCATION},requestPermissionCode);
+    }
+
+    public void onConnctionFailed (@NonNull ConnectionResult connectionResult)
+    {
+        aletUserAboutError();
+    }
+
+    private void getForcast(double latitude, double longitude)
+    {
         final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
 
         TextView darkSky = findViewById(R.id.darkSkyAttribution);
@@ -135,7 +173,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException
+    {
         JSONObject forcast = new JSONObject(jsonData);
 
         String timezone = forcast.getString("timezone");
@@ -159,7 +198,8 @@ public class MainActivity extends AppCompatActivity {
         return currentWeather;
     }
 
-    private boolean isNetwotkAvailable() {
+    private boolean isNetwotkAvailable()
+    {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 
@@ -179,7 +219,71 @@ public class MainActivity extends AppCompatActivity {
     }
     public void refreshOnClick (View view)
     {
-        getForcast(latitude,longitude);
-
+        getForcast(getLatitude(),getLongitude());
     }
+
+    private double[] getParmeters()
+    {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null)
+                        {
+                            getCityName(location, new OnGeocoderFinishedListener() {
+                                @Override
+                                public void onFinished(List<Address> results) {
+                                    // do something with the result
+                                    city=results.toString();
+                                }
+                            });
+
+                            arry[0]=location.getLatitude();
+                            arry[1]=location.getLongitude();
+                        }
+                    }
+                });
+
+        Log.i(TAG,"Location parameters:"+String.valueOf(arry[0])+","+String.valueOf(arry[1]));
+        return arry;
+    }
+
+    public double getLatitude()
+    {
+        getParmeters();
+        return arry[0];
+    }
+
+    public double getLongitude()
+    {
+        getParmeters();
+        return arry[1];
+    }
+
+
+    public void getCityName(final Location location, final OnGeocoderFinishedListener listener) {
+        new AsyncTask<Void, Integer, List<Address>>() {
+            @Override
+            protected List<Address> doInBackground(Void... arg0) {
+                Geocoder coder = new Geocoder(getBaseContext(), Locale.ENGLISH);
+                List<Address> results = null;
+                try {
+                    results = coder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                } catch (IOException e) {
+                    // nothing
+                }
+                return results;
+            }
+
+            @Override
+            protected void onPostExecute(List<Address> results) {
+                if (results != null && listener != null) {
+                    listener.onFinished(results);
+                }
+            }
+        }.execute();
+    }
+
 }
